51129fc4dfae5dd509e240cc72c91e4341ba2fe5
黄程 <huangcheng@huangchengdeMacBook-Pro.local>
2018-03-19 22:59:01 +0800

    token

--- koa-demo/controller/user.controller.js
+++ koa-demo/controller/user.controller.js
@@ -28,31 +28,77 @@ class UserController extends BasController {
     changeUser() {
 
     }
-    async login(options) {
-        let userInfo = await this.service.getItem({
-            nikeName: options.nikeName
-        });
-        if (userInfo.isSuccess) {
-            if (userInfo.data.psw === options.psw) {
-                let tokenInfo = await this.service.getToken();
-                if(tokenInfo.isSuccess){
-                    //
-                }else{
-                    //
-                }
+    /**
+     * 登陆主要处理登陆问题
+     * @param {obj} options 
+     */
+    login(options) {
+        return async () => {
+            let userInfo = await this.service.getItem({
+                nikeName: options.nikeName
+            });
+            if (userInfo.isSuccess) {
+                if (userInfo.data.psw === options.psw) {
+                    let tokenInfo = await this.service.getToken({
+                        userId: userInfo.data.id
+                    });
+                    if (tokenInfo.isSuccess) {
+                        //获取token
+                        return {
+                            isSuccess: true,
+                            data: {
+                                token: tokenInfo.data.id
+                            }
 
+                        };
+                    } else {
+
+                        /*
+                         * 这里就要处理第一次登陆进来的用户了，理论上应该是第一次注册的时候就直接登陆进去创建一次token的
+                         * 这里还要处理更新token
+                         * 登出的时候是否处理成把时间换成现在就好了
+                         */
+                        // return {
+                        //     isSuccess: false,
+                        //     msg: '获取token失败！'
+                        // };
+                        
+                    }
+                } else {
+                    return {
+                        isSuccess: false,
+                        msg: '密码错误！'
+                    };
+                }
             } else {
                 return {
-                    isSuccess:false,
-                    msg:'密码错误！'
+                    isSuccess: false,
+                    msg: '用户名未注册！'
                 };
             }
-        } else {
-            return {
-                isSuccess:false,
-                msg:'用户名未注册！'
-            };
-        }
+        };
+
+    }
+    /**
+     * 
+     * @param {Obj} options 
+     */
+    logout(options) {
+
+        return  async ()=>{
+            let logout = await this.service.logout();
+            if(logout.isSuccess){
+                return {
+                    isSuccess:true,
+                    data:'登出成功！'
+                };
+            }else{
+                return {
+                    isSuccess:false,
+                    msg:'登出失败了！'
+                };
+            }
+        };
     }
 
 }
