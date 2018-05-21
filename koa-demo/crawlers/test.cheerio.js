35584d726cfb435c08c8c664bfeb259a5b1246a2
yaoyunchou <303630573@qq.com>
2018-05-21 19:18:55 +0800

    加入爬虫‘

--- koa-demo/crawlers/test.cheerio.js
+++ koa-demo/crawlers/test.cheerio.js
@@ -1,2 +1,45 @@
-const cheerio = require('cheerio');  
+const cheerio = require('cheerio');
+const http = require('http');
+const superagent = require('superagent');
 
+
+
+
+
+
+
+
+
+//测试获取的列表数据
+
+class Crawlers {
+    constructor() {
+
+    }
+    getList() {
+
+    }
+    getDetail() {
+        try {
+            let url = encodeURI('/food/search?keyword=鸡蛋');
+            superagent.get('http://www.boohee.com/food/search?keyword=%E5%9C%9F%E8%B1%86').end(function (err, res) {
+                // 抛错拦截
+                if (err) {
+                    throw Error(err);
+                }
+                let _$ = cheerio.load(res.text);
+            });
+        } catch (error) {
+            console.log(error);
+        }
+
+    }
+    getHttpDetail() {
+        http.get();
+    }
+}
+
+
+const crawlers = new Crawlers();
+
+crawlers.getDetail();
